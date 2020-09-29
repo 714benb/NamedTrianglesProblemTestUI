70620ac6e9a72cdf46d0403e804ef74ff938ff5d
MithralSmith <TheSmith@MithralSmith>
2020-09-29 09:51:57 -0700

    Added ASP.NET MVS Core project to test the NamedTriangleProblem.  It used JQuery to access the api. Note: the API has to allow CORS access to this site's requests.

---
+++ NamedTrianglesProblemTestUI/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NamedTrianglesProblemTestUI.Controllers
+{
+  [Route("demo")]
+  public class DemoController : Controller
+  {
+    [Route("")]
+    [Route("index")]
+    [Route("~/")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [Route("demo1")]
+    public IActionResult Demo1()
+    {
+      return new JsonResult("Hello");
+    }
+
+    [Route("demo2/{fullName}")]
+    public IActionResult Demo2(string fullName)
+    {
+      return new JsonResult("Hello " + fullName);
+    }
+  }
+}
---
+++ NamedTrianglesProblemTestUI/Models/TriangleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Intrinsics;
+using System.Threading.Tasks;
+using GeometricLayouts.Models;
+
+namespace NamedTrianglesProblemTestUI.Models
+{
+  public class TriangleModel
+  {
+
+    [Display(Name="Vertex 1")]
+    public Vertex V1 { get; set; }
+    [Display(Name="Vertex 2")]
+    public Vertex V2 { get; set; }
+    [Display(Name="Vertex 3")]
+    public Vertex V3 { get; set; }
+
+    [Display(Name="Name")]
+    public string Name { get; set; }
+  }
+}
